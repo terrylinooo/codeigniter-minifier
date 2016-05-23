04cf1e1a757bb5cb71552b3f9254eeee69a5a9b3
Terry Lin <some.lin@gmail.com>
2016-05-23 22:46:52 +0800

    Create hooks.php

---
+++ application/config/hooks.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------------
+| Hooks
+| -------------------------------------------------------------------------
+| This file lets you define "hooks" to extend CI without hacking the core
+| files.  Please see the user guide for info:
+|
+|	http://codeigniter.com/user_guide/general/hooks.html
+|
+*/
+
+$hook['display_override'][] = array(
+    'class' => '',
+    'function' => 'CI_Minifier_Hook_Loader',
+    'filename' => '',
+    'filepath' => ''
+);
